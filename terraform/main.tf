6f3791aaa6d956fa1ab5703f46808a7d11823165
dlicalzi <davidlicalzi@yahoo.com>
2024-05-29 11:34:55 -0400

    updated s3, cf, and changed main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,28 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+module "cloudfront" {
+  source              = "./modules/cloudfront"
+  origin_bucket       = module.s3_static_website.bucket_name
+  acm_certificate_arn = "arn:aws:acm:us-east-1:785169158894:certificate/ddf9372e-77f0-4d27-96a7-b57c44b0f20a"
+  aliases             = ["team-cuttlefish.aws-tfbd.com"]
+  
+}
+
+
+module "s3_static_website" {
+  source = "./modules/s3_bucket"
+
+  bucket_name                           = "teamcuttlefish"
+  cloudfront_origin_access_identity_arn = module.cloudfront.origin_access_identity_arn
+  
+}
+
+output "s3_bucket_name" {
+  value = module.s3_static_website.bucket_name
+}
+
+output "cloudfront_domain_name" {
+  value = module.cloudfront.cloudfront_domain_name
+}
